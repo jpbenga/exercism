59061c7db28c14f6ed16dec75263b58c7b96c979
jpbenga <jpaulbenga@gmail.com>
2024-04-26 16:46:53 +0200

    leap solution

--- leap/leap.ts
+++ leap/leap.ts
@@ -1,3 +1,6 @@
-export function isLeap() {
-  throw new Error('Remove this statement and implement this function')
+export function isLeap(year: number): boolean {
+   if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)){
+        return true;
+    }
+    return false;
 }
