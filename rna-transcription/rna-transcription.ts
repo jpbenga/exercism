ff33942d646878ec2a8bfd3a4499efd97aadcf55
jpbenga <jpaulbenga@gmail.com>
2024-04-28 10:34:06 +0200

    nucleotides solution

--- rna-transcription/rna-transcription.ts
+++ rna-transcription/rna-transcription.ts
@@ -1,3 +1,20 @@
-export function toRna() {
-  throw new Error('Remove this statement and implement this function')
+export function toRna(dna: string): string {
+  let rna: string = "";
+  // Il faut récupérer chaque nucleotides de la séquence dna
+  for(let char of dna){
+    if (char in nucleotides){
+        rna += nucleotides[char];
+    } else {
+        throw new Error('Invalid input DNA.');       
+    }
+}
+return rna;
+}
+
+type Nucleotides = Record<string, string>;
+const nucleotides: Nucleotides = {
+    "G": "C",
+    "C": "G",
+    "T": "A",
+    "A": "U",
 }
